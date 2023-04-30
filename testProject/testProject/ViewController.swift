7f7aff0574876190ffeb342f56eb17d83d1ede67
Melchik <mmmelchik@yandex.ru>
2023-04-30 11:47:43 +0300

    Test

---
+++ testProject/testProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testProject
+//
+//  Created by Илья Поздняков on 30.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
